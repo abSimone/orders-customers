5e500feb89ef539607ebe1810883e55f8f4d98c9
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-11-03 15:30:38 +0100

    fix e aggiunta metodi ordersDao

--- dao/ordersDao.py
+++ dao/ordersDao.py
@@ -13,3 +13,29 @@ class Order:
         MySql.closeConnection()
 
         return data
+
+    @classmethod
+    def getOrderStatusByOrderNumber(cls, orderNumber):
+        MySql.openConnection()
+        MySql.query(f"""
+                     SELECT *\
+                     FROM orders\
+                     WHERE orderNumber = {orderNumber}
+                     """)
+        data = MySql.getResults()
+        MySql.closeConnection()
+
+        return data
+
+    @classmethod
+    def getAllInfosByProductCode(cls, orderNumber):
+        MySql.openConnection()
+        MySql.query(f"""
+                     SELECT o.*, od.productCode, od.quantityOrdered, od.priceEach, od.orderLineNumber\
+                     FROM orders o, orderdetails od\
+                     WHERE o.orderNumber = {orderNumber} and od.orderNumber = o.orderNumber
+                     """)
+        data = MySql.getResults()
+        MySql.closeConnection()
+
+        return data
