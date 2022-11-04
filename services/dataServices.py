a79e98a29480ebcb7878932e0b23557979b53872
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-11-04 14:23:06 +0100

    implementazione metodi dataServices per ordini

---
+++ services/dataServices.py
@@ -0,0 +1,52 @@
+from dto.ordersDto import OrderDto
+from dto.orderdetailsDto import OrderDetailsDto
+
+class Services:
+
+    @classmethod
+    def findAllOrdersService(cls):
+        return OrderDto().findAllOrders()
+
+    @classmethod
+    def findOrderStatusService(cls, orderNumber : int):
+        return OrderDto().findOrderStatus(orderNumber)
+
+    @classmethod
+    def findArticleStatusService(cls, orderNumber : int):
+        return OrderDto().findArticleStatus(orderNumber)
+
+    @classmethod
+    def findAllOrderDetailsService(cls):
+        return OrderDetailsDto().findAllOrderDetails()
+
+    @classmethod
+    def findAllOrdersArticlesNumberService(cls):
+        return OrderDetailsDto().findAllOrdersArticlesNumber()
+
+    @classmethod
+    def findAllOrdersArticlesNumberByOrderNumberService(cls, orderNumber : int):
+        return OrderDetailsDto().findAllOrdersArticlesNumberByOrderNumber(orderNumber)
+    
+    @classmethod
+    def findAllArticlesQuantityOrderedService(cls):
+        return OrderDetailsDto().findAllArticlesQuantityOrdered()
+
+    @classmethod
+    def findQuantityOrderedByArticleService(cls, productCode : str):
+        return OrderDetailsDto().findQuantityOrderedByArticle(productCode)
+
+    @classmethod
+    def findAllArticlesTotalEarningsService(cls):
+        return OrderDetailsDto().findAllArticlesTotalEarnings()
+
+    @classmethod
+    def findTotalEarningsByArticleService(cls, productCode : str):
+        return OrderDetailsDto().findTotalEarningsByArticle(productCode)
+
+    @classmethod
+    def findAllOrdersTotalEarningsService(cls):
+        return OrderDetailsDto().findAllOrdersTotalEarnings()
+
+    @classmethod
+    def findTotalEarningsByOrderService(cls, orderNumber : int):
+        return OrderDetailsDto().findTotalEarningsByOrder(orderNumber)
